e973cccb9cbd982e0d82e6370dca06412567685b
Nikki <nikkipetzer@gmail.com>
2025-08-13 15:02:15 -0400

    Add day and duration functions: get_project_duration(), each_day(), middle_days()

--- reimbursement_calculator.py
+++ reimbursement_calculator.py
@@ -1,7 +1,12 @@
+#Imports
+from datetime import date, datetime, timedelta
+
+#Define Reimbursement Rate Table
 reimbursement_rates = {
     "low": {"travel": 45, "full": 75},
     "high": {"travel": 55, "full": 85}
 }
+
 
 def get_reimbursement_rate(city_cost: str, day_type: str):
     """Get the reimbursement rate based on city cost and day type."""
@@ -14,3 +19,22 @@ def get_reimbursement_rate(city_cost: str, day_type: str):
         raise ValueError(f"Invalid day type: {day_type}. Valid options are: {list(reimbursement_rates[city_cost_cleaned])}")
     
     return reimbursement_rates[city_cost_cleaned][day_type_cleaned]
+
+
+def get_project_duration(start_date: date, end_date: date):
+    """Calculate the day count between two project dates (inclusive)."""
+    if end_date < start_date:
+        raise ValueError("End date cannot be before start date. It must equal or follow the start date.")
+    return (end_date - start_date).days + 1
+
+
+def each_day(start_date: date, end_date: date):
+    """Return each day in a project period start_date to end_date (inclusive)."""
+    for day in range(get_project_duration(start_date, end_date)):
+        yield start_date + timedelta(days=day)
+
+
+def middle_days(start_date: date, end_date: date):
+    """Return all days in a project period except the first and last day: the middle days in the period."""
+    for day in range(1, get_project_duration(start_date, end_date) - 1):
+        yield start_date + timedelta(days=day)
